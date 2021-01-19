f59dbead5b2c31ab8f3cdc59dc2c52d2fdb24141
usuario <usuario@DESKTOP-T6UB5T5>
2021-01-19 20:17:25 +0100

    commit

--- resources/views/animales/index.blade.php
+++ resources/views/animales/index.blade.php
@@ -5,5 +5,21 @@
 @section('contenido')
     <h1>Listado de Animales</h1>
 
+<div class="row">
+@foreach( $animales as $clave => $animal )
+<div class="card" style="width: 18rem;">
+<img src="{{asset('assets/imagenes')}}/{{$animal['imagen']}}" style="height:200px"/>
+  <div class="card-body">
+      <h5 class="card-title"><a href="{{ route('animales.show' , $clave ) }}"></a></h5>
+    
+    <p class="card-text">- {{$animal['especie']}}</p>
+    <p class="card-text">- {{$animal['peso']}}</p>
+    <p class="card-text">- {{$animal['fechaNacimiento']}}</p>
+    <p class="card-text">- {{$animal['alimentacion']}}</p>
+    
+    <a href="{{route('animales.show' , $clave )}}"  class="btn btn-primary">Ver Animal</a>
+  </div>
+</div>
+@endforeach
+</div>
 @endsection
-
